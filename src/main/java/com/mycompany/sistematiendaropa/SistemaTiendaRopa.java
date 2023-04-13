0bf41986be3a934a115499c59ad359cf9e7c3e5b
Sergio Nicolás Moyano (Backend Developer) <107949152+Snmoyano@users.noreply.github.com>
2023-04-13 12:06:51 -0300

    upload

---
+++ src/main/java/com/mycompany/sistematiendaropa/SistemaTiendaRopa.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistematiendaropa;
+
+/**
+ *
+ * @author nicolas
+ */
+public class SistemaTiendaRopa {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        new frmSistemaTiendaRopa().setVisible(true);
+    }
+}
